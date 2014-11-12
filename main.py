e0a74bd6cb59514084b6abc049fbe5944670d2a3
Matt Barnes <barnes.matt.j@gmail.com>
2014-11-12 08:49:57 -0500

    Shuffled training data.

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 __author__ = 'mbarnes1'
 from online_logistic_regression import OnlineLogisticRegression
 import numpy as np
-
+from itertools import izip
+import random
 
 def main():
     """
     Opens data log and runs online logistic regression
     """
-    log_reg = OnlineLogisticRegression(alpha=0.001, number_classes=5, number_features=10)
-    labels_true = list()
+    log_reg = OnlineLogisticRegression(alpha=0.1, number_classes=5, number_features=10)
     ins = open('data/oakland_part3_am_rf.node_features')
     results = open('results.txt', 'w')
     next(ins)  # first header line
     next(ins)  # first header line
     next(ins)  # first header line
+    features = list()
+    labels_true = list()
     for counter, line in enumerate(ins):
-        print 'Sample:', counter
+        print 'Loading sample:', counter
         data = [float(x) for x in line.split()]
-        label = map_label(int(data[4]))
-        features = np.array(data[5:])
-        log_reg.update(features, label)
-        labels_true.append(label)
-    accuracy = float(sum(np.array(labels_true) == np.array(log_reg.predictions)))/len(labels_true)
-    results.write('*** Online Results *** \nNet accuracy: ' + str(accuracy) + '\n')
+        labels_true.append(map_label(int(data[4])))
+        features.append(np.array(data[5:]))
+    dual_shuffle(features, labels_true)
+    for counter, (feature, label) in enumerate(izip(features, labels_true)):
+        print 'Training sample:', counter
+        log_reg.update(feature, label)
+    correct = sum(np.array(labels_true) == np.array(log_reg.predictions))
+    accuracy = float(correct)/len(labels_true)
+    results.write('*** Online Results *** \nNet accuracy: ' + str(correct) + '/' + str(len(labels_true)) + ' = ' + str(accuracy) + '\n')
     class_accuracies = class_accuracy(labels_true, log_reg.predictions)
     name_accuracies = {map_name(label): acc for label, acc in class_accuracies.iteritems()}
-    for name, accuracy in name_accuracies.iteritems():
-        results.write(name + ': ' + str(accuracy) + '\n')
+    for name, (correct, occurences, accuracy) in name_accuracies.iteritems():
+        results.write(name + ': ' + str(correct) + '/' + str(occurences) + ' = ' + str(accuracy) + '\n')
     ins = open('data/oakland_part3_an_rf.node_features')
     next(ins)  # first header line
     next(ins)  # first header line
@@ -34,18 +39,30 @@ def main():
     heldout_true = list()
     heldout_predicted = list()
     for counter, line in enumerate(ins):
-        print 'Sample:', counter
+        print 'Testing sample:', counter
         data = [float(x) for x in line.split()]
         label = map_label(int(data[4]))
         features = np.array(data[5:])
         heldout_predicted.append(log_reg.predict(features))
         heldout_true.append(label)
-    accuracy = float(sum(np.array(heldout_true) == np.array(heldout_predicted)))/len(heldout_true)
-    results.write('\n*** Held-out Results *** \nNet accuracy: ' + str(accuracy) + '\n')
+    correct = sum(np.array(heldout_true) == np.array(heldout_predicted))
+    accuracy = float(correct)/len(heldout_true)
+    results.write('\n*** Held-out Results *** \nNet accuracy: ' + str(correct) + '/' + str(len(heldout_true)) + ' = ' + str(accuracy) + '\n')
     class_accuracies = class_accuracy(heldout_true, heldout_predicted)
     name_accuracies = {map_name(label): acc for label, acc in class_accuracies.iteritems()}
-    for name, accuracy in name_accuracies.iteritems():
-        results.write(name + ': ' + str(accuracy) + '\n')
+    for name, (correct, occurences, accuracy) in name_accuracies.iteritems():
+        results.write(name + ': ' + str(correct) + '/' + str(occurences) + ' = ' + str(accuracy) + '\n')
+
+
+def dual_shuffle(list_a, list_b):
+    """
+    Shuffles two lists in place such that order is maintained
+    :param list_a:
+    :param list_b:
+    """
+    combined = zip(list_a, list_b)
+    random.shuffle(combined)
+    list_a[:], list_b[:] = zip(*combined)
 
 
 def class_accuracy(labels_true, labels_predicted):
@@ -53,7 +70,7 @@ def class_accuracy(labels_true, labels_predicted):
     Calcultes the accuracy of each class
     :param labels_true: Vector of class labels
     :param labels_predicted: Vector of predicted labels
-    :return accuracies: List of accuracies for each class
+    :return accuracies: Dictionary of [class, (correct, occurences, accuracy)]
     """
     labels_true = np.array(labels_true)
     labels_predicted = np.array(labels_predicted)
@@ -65,7 +82,7 @@ def class_accuracy(labels_true, labels_predicted):
         true_indices = labels_true == cls
         occurences = sum(true_indices)
         correct = sum(labels_predicted[true_indices] == cls)
-        accuracies[cls] = float(correct)/occurences
+        accuracies[cls] = (correct, occurences, float(correct)/occurences)
     return accuracies
 
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
-from main import class_accuracy
+from main import class_accuracy, dual_shuffle
+from copy import deepcopy
 __author__ = 'mbarnes1'
 
 
@@ -19,6 +20,22 @@ class MyTestCase(unittest.TestCase):
             3: 0.5,
         }
         self.assertEqual(accuracies, correct)
+
+    def test_dual_shuffle(self):
+        a = ['Spears', "Adele", "NDubz", "Nicole", "Cristina"]
+        a_copy = deepcopy(a)
+        b = [1, 2, 3, 4, 5]
+        b_copy = deepcopy(b)
+        self.assertEqual(a, a_copy)
+        self.assertEqual(b, b_copy)
+        dual_shuffle(a, b)
+        self.assertNotEqual(a, a_copy)
+        self.assertNotEqual(b, b_copy)
+        self.assertEqual(b_copy, sorted(b))
+        indices = np.argsort(b)
+        a_sorted = list(np.array(a)[indices])
+        self.assertEqual(a_sorted, a_copy)
+
 
 if __name__ == '__main__':
     unittest.main()
